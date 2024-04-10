2fb87b3f81a617c3e7eb8a7e7ca6c57eb126072d
lilansheng <945004642@qq.com>
2024-04-10 11:02:03 +0800

    React.memo - props的比较机制

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { memo, useMemo, useState } from 'react'
 
-function Son(params) {
+function Son({ count }) {
   console.log('Son重新渲染了')
-  return <div>son</div>
+  return <div>son{count}</div>
 }
 const MemoSon = memo(Son)
 function App() {
+  console.log('App重新渲染了')
   const [count, setCount] = useState(0)
-
+  const num = 100
+  const arr = [1, 2, 3]
+  const newArr = useMemo(() => arr, [])
   return (
     <div className="App">
-      <Son />
-      <MemoSon />
+      {/* <Son /> */}
+      {/* <MemoSon count={count} /> */}
+      {/* <MemoSon count={num} /> */}
+      {/* <MemoSon count={arr} /> */}
+      <MemoSon count={newArr} />
       <button onClick={() => setCount(count + 1)}>+</button>
       {count}
     </div>
