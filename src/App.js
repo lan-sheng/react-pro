ab107214c9f05642ef380d0bf262acae013f6fa3
lilansheng <945004642@qq.com>
2024-04-11 18:18:28 +0800

    zustand-异步支持

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { useEffect } from 'react'
 import { create } from 'zustand'
+const URL = 'http://geek.itheima.net/v1_0/channels'
+
 
 const useStore = create(set => {
   return {
@@ -6,12 +9,33 @@ const useStore = create(set => {
     inc: () => {
       set(state => ({ count: state.count + 1 }))
     },
+    channelList: [],
+    fetchGetList: async () => {
+      const res = await fetch(URL)
+      const jsonRes = await res.json()
+      console.log('jsonRes: ', jsonRes)
+      set({
+        channelList: jsonRes.data.channels
+      })
+    },
   }
 })
 
-function App () {
-  const {count,inc}=useStore()
-  return <><button onClick={inc}>{ count}</button></>
+function App() {
+  const { count, inc, channelList, fetchGetList } = useStore()
+  useEffect(() => {
+    fetchGetList()
+  }, [fetchGetList])
+  return (
+    <>
+      <button onClick={inc}>{count}</button>
+      <ul>
+        {
+          channelList.map(item => <li key={item.id}>{item.name}</li>)
+        }
+      </ul>
+    </>
+  )
 }
 
 export default App
